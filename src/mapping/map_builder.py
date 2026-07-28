7208b5103aeee7cf9d4136ae6255f6e115c33e25
hieu <23021548@vnu.edu.vn>
2026-07-28 15:06:51 +0700

    fix: correct choosing largest connected components

--- src/mapping/map_builder.py
+++ src/mapping/map_builder.py
@@ -4,15 +4,22 @@ import sys
 import os
 import pickle
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(ROOT_DIR)
 import config
+
+GRAPH_PATH = os.path.join(ROOT_DIR, "data", "processed", "cau_giay_graph.pkl")
 
 def build_cau_giay_graph():
     G = ox.graph_from_place(config.PLACE_NAME, network_type=config.NETWORK_TYPE)
-    print(f"Download map sucess with {len(G.nodes)} nodes and {len(G.edges)} edges.")
+    print(f"Download map success with {len(G.nodes)} nodes and {len(G.edges)} edges.")
     
     G = ox.add_edge_speeds(G, fallback=config.DEFAULT_SPEED_KMH)
     G = ox.add_edge_travel_times(G)
+
+    largest_scc = max(nx.strongly_connected_components(G), key=len)
+    G = G.subgraph(largest_scc).copy()
+    print(f"After SCC filter: {len(G.nodes)} nodes, {len(G.edges)} edges.")
     
     station_nodes = {}
     for name, coords in config.VINFAST_STATIONS.items():
@@ -21,18 +28,15 @@ def build_cau_giay_graph():
         
         G.nodes[nearest_node]['is_charging_station'] = True
         G.nodes[nearest_node]['station_name'] = name
-
-        largest_scc = max(nx.strongly_connected_components(G), key=len)
-        G = G.subgraph(largest_scc).copy()
     
     return G, station_nodes
 
 if __name__ == "__main__":
     graph, stations = build_cau_giay_graph()
-    
-    os.makedirs("data/processed", exist_ok=True)
-    
-    with open("data/processed/cau_giay_graph.pkl", "wb") as f:
-        pickle.dump(graph, f)
-        
-    print(f"Graph saved")
+
+    os.makedirs(os.path.dirname(GRAPH_PATH), exist_ok=True)
+
+    with open(GRAPH_PATH, "wb") as f:
+        pickle.dump((graph, stations), f)
+
+    print(f"Graph saved to {GRAPH_PATH}")
