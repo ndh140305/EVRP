922cabcc20f4b948c27e96045eb2fe29f2aad0bc
hieu <23021548@vnu.edu.vn>
2026-07-28 15:42:59 +0700

    fix: set depot at 302 Cau GIay

--- config.py
+++ config.py
@@ -4,14 +4,21 @@ NETWORK_TYPE = "drive"
 
 DEFAULT_SPEED_KMH = 30.0
 
+DEPOT = {
+    "id": "Depot",
+    "lat": 21.0267,
+    "lon": 105.7986,
+    "name": "302 Cau Giay - Discovery Complex"
+}
+
 VINFAST_STATIONS = {
-    "Station_1": [21.0365, 105.7832],  # Xuân Thủy
-    "Station_2": [21.0264, 105.7971],  # Trần Duy Hưng
-    "Station_3": [21.0402, 105.7958],  # Cầu Giấy
-    "Station_4": [21.0225, 105.7865],  # Dương Đình Nghệ
-    "Station_5": [21.0301, 105.7922],  # Công viên Cầu Giấy
-    "Station_6": [21.0478, 105.7995],  # Ngoại Giao Đoàn / Nghĩa Đô
-    "Station_7": [21.0335, 105.7725],  # Duy Tân / Cốm Vòng
-    "Station_8": [21.0155, 105.8021],  # Khu Trung Hòa Nhân Chính
-    "Station_9": [21.0425, 105.7890]   # Nghĩa Đô
+    "Station_1": [21.0365, 105.7832],
+    "Station_2": [21.0264, 105.7971],
+    "Station_3": [21.0402, 105.7958],
+    "Station_4": [21.0225, 105.7865],
+    "Station_5": [21.0301, 105.7922],
+    "Station_6": [21.0478, 105.7995],
+    "Station_7": [21.0335, 105.7725],
+    "Station_8": [21.0155, 105.8021],
+    "Station_9": [21.0425, 105.7890]
 }
