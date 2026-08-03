55b1c7daf14ba4b5f91b7c74c510bf2f5a957679
hieu <23021548@vnu.edu.vn>
2026-08-03 22:27:31 +0700

    update vehicle and station specs

--- config.py
+++ config.py
@@ -1,24 +1,56 @@
 PLACE_NAME = "Cau Giay District, Hanoi, Vietnam"
-
 NETWORK_TYPE = "drive"
 
-DEFAULT_SPEED_KMH = 30.0
+VEHICLE_SPEC = {
+    "battery_kwh": 5.0,
+    "consumption_kwh_per_km": 0.18,
+    "max_load_kg": 500,
+    "min_battery_safety_percent": 0.10
+}
 
 DEPOT = {
-    "id": "Depot",
+    "id": "D0",
     "lat": 21.0267,
     "lon": 105.7986,
-    "name": "302 Cau Giay - Discovery Complex"
+    "name": "302 Cau Giay - Discovery Complex",
+    "tw_open": 480,
+    "tw_close": 1020
 }
 
 VINFAST_STATIONS = {
-    "Station_1": [21.0365, 105.7832],
-    "Station_2": [21.0264, 105.7971],
-    "Station_3": [21.0402, 105.7958],
-    "Station_4": [21.0225, 105.7865],
-    "Station_5": [21.0301, 105.7922],
-    "Station_6": [21.0478, 105.7995],
-    "Station_7": [21.0335, 105.7725],
-    "Station_8": [21.0155, 105.8021],
-    "Station_9": [21.0425, 105.7890]
+    "S1": {
+        "name": "Tram sac VinFast - Xuan Thuy",
+        "lat": 21.0365,
+        "lon": 105.7832,
+        "charging_rate_kw": 22.0
+    },
+    "S2": {
+        "name": "Tram sac VinFast - Tran Duy Hung",
+        "lat": 21.0264,
+        "lon": 105.7971,
+        "charging_rate_kw": 22.0
+    },
+    "S3": {
+        "name": "Tram sac VinFast - Cau Giay",
+        "lat": 21.0402,
+        "lon": 105.7958,
+        "charging_rate_kw": 11.0
+    },
+    "S4": {
+        "name": "Tram sac VinFast - Duong Dinh Nghe",
+        "lat": 21.0225,
+        "lon": 105.7865,
+        "charging_rate_kw": 50.0
+    },
+    "S5": {
+        "name": "Tram sac VinFast - Cong vien Cau Giay",
+        "lat": 21.0301,
+        "lon": 105.7922,
+        "charging_rate_kw": 22.0
+    }
+}
+
+SYSTEM_PARAMS = {
+    "speed_kmh": 40.0,
+    "charging_policy": "full_charge"
 }
